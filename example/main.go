0e01d461ade7a0a1536fb4848686f150fc1e9c96
lemo <lemo@gamstudio.net>
2019-10-26 19:12:45 +0800

    init

---
+++ example/main.go
@@ -0,0 +1,38 @@
+/**
+* @program: mongo
+*
+* @description:
+*
+* @author: lemo
+*
+* @create: 2019-10-26 19:10
+**/
+
+package main
+
+import (
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	"mongo"
+)
+
+func main() {
+	var url = "mongodb://root:1354243@127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019"
+
+	mgo, _ := mongo.NewMongoClient().Connect(&mongo.Config{Url: url})
+
+	err := mgo.RawClient().Ping(nil, mongo.ReadPreference.Primary)
+	if err != nil {
+		panic(err)
+	}
+
+	var res = []bson.M{nil}
+
+	_ = mgo.DB("QGame").C("GameUser").Find(bson.M{}).All(&res)
+
+	for key, value := range res {
+		log.Println(key, value)
+	}
+}
