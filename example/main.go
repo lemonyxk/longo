442687b541f52fd110b713dffafb7db12d2f7942
lemo <lemo@gamstudio.net>
2019-10-28 17:02:56 +0800

    init

--- example/main.go
+++ example/main.go
@@ -12,29 +12,29 @@ package main
 
 import (
 	"go.mongodb.org/mongo-driver/bson"
-	
-	"mongo"
+
+	"github.com/Lemo-yxk/mongo"
 )
 
 func main() {
 	var url = "mongodb://root:1354243@127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019"
-	
+
 	mgo, _ := mongo.NewMongoClient().Connect(&mongo.Config{Url: url})
-	
+
 	err := mgo.RawClient().Ping(nil, mongo.ReadPreference.Primary)
 	if err != nil {
 		panic(err)
 	}
-	
+
 	mgo.Transaction(func(sessionContext mongo.SessionContext) error {
-		
+
 		var err error
-		
+
 		_, err = mgo.DB("QGame").C("test1").InsertOneWithSession(sessionContext, bson.M{"hello": "world"})
-		
+
 		_, err = mgo.DB("QGame").C("test2").InsertOneWithSession(sessionContext, bson.M{"hello": "world"})
-		
+
 		return err
 	})
-	
+
 }
